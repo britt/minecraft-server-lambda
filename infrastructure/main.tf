f0aa26f6b6e2354cc3db0340d7c950cd95b7ca3a
Britt Crawford <britt.v.crawford@gmail.com>
2019-03-21 14:49:05 -0700

    add API gateway integration

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -14,6 +14,15 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
+data "aws_lambda_function" "start_function" {
+  function_name = "${var.apex_function_names["start"]}"
+}
+
+data "aws_lambda_function" "stop_function" {
+  function_name = "${var.apex_function_names["stop"]}"
+}
+
+
 resource "aws_iam_policy" "ec2-start-stop" {
   name        = "ec2-start-stop"
     description = "Allow lambda functions to start and stop EC2 instances"
@@ -32,11 +41,95 @@ resource "aws_iam_policy" "ec2-start-stop" {
         }
     ]
 }
-    EOF
+  EOF
 }
 
 resource "aws_iam_policy_attachment" "ec2-start-stop-lambda-attach" {
     name       = "ec2-start-stop-attachment"
     roles      = ["${var.project_name}_lambda_function"]
     policy_arn = "${aws_iam_policy.ec2-start-stop.arn}"
+}
+
+resource "aws_api_gateway_rest_api" "mc-control" {
+  name        = "MinecraftServerControl"
+  description = "Minecraft Server Control"
+}
+
+resource "aws_api_gateway_resource" "start" {
+  rest_api_id = "${aws_api_gateway_rest_api.mc-control.id}"
+  parent_id   = "${aws_api_gateway_rest_api.mc-control.root_resource_id}"
+  path_part   = "start"
+}
+
+resource "aws_api_gateway_resource" "stop" {
+  rest_api_id = "${aws_api_gateway_rest_api.mc-control.id}"
+  parent_id   = "${aws_api_gateway_rest_api.mc-control.root_resource_id}"
+  path_part   = "stop"
+}
+
+resource "aws_api_gateway_method" "start_method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.mc-control.id}"
+  resource_id   = "${aws_api_gateway_resource.start.id}"
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "stop_method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.mc-control.id}"
+  resource_id   = "${aws_api_gateway_resource.stop.id}"
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "start_lambda" {
+  rest_api_id = "${aws_api_gateway_rest_api.mc-control.id}"
+  resource_id = "${aws_api_gateway_resource.start.id}"
+  http_method = "${aws_api_gateway_method.start_method.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${data.aws_lambda_function.start_function.invoke_arn}"
+}
+
+resource "aws_api_gateway_integration" "stop_lambda" {
+  rest_api_id = "${aws_api_gateway_rest_api.mc-control.id}"
+  resource_id = "${aws_api_gateway_resource.stop.id}"
+  http_method = "${aws_api_gateway_method.stop_method.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${data.aws_lambda_function.stop_function.invoke_arn}"
+}
+
+resource "aws_lambda_permission" "start_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "${var.apex_function_names["start"]}"
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the API Gateway "REST API".
+  // source_arn = "${var.apex_function_arns["start"]}/*/*"
+}
+
+resource "aws_lambda_permission" "stop_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "${var.apex_function_names["stop"]}"
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the API Gateway "REST API".
+  // source_arn = "${var.apex_function_arns["stop"]}/*/*"
+}
+
+resource "aws_api_gateway_deployment" "mc_control_deployment" {
+  depends_on = ["aws_api_gateway_integration.start_lambda", "aws_api_gateway_integration.stop_lambda"]
+
+  rest_api_id = "${aws_api_gateway_rest_api.mc-control.id}"
+  stage_name  = "production"
+}
+
+output "base_url" {
+  value = "${aws_api_gateway_deployment.mc_control_deployment.invoke_url}"
 }
